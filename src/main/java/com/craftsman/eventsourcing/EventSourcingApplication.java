5093317c96af797d69fe862d459ac6621be19755
jokefaker <jokefaker@gmail.com>
2019-06-06 17:08:30 +0800

    init

---
+++ src/main/java/com/craftsman/eventsourcing/EventSourcingApplication.java
@@ -0,0 +1,13 @@
+package com.craftsman.eventsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSourcingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSourcingApplication.class, args);
+	}
+
+}
