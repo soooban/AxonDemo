396a8e2f53f3acf02197c24ab3688e2b7ce21a95
jokefaker <jokefaker@gmail.com>
2019-06-12 11:27:43 +0800

    add snapshot

--- src/main/java/com/craftsman/eventsourcing/es/continuance/producer/config/AxonContinueConfiguration.java
+++ src/main/java/com/craftsman/eventsourcing/es/continuance/producer/config/AxonContinueConfiguration.java
@@ -8,6 +8,7 @@ import org.axonframework.common.jdbc.PersistenceExceptionResolver;
 import org.axonframework.common.jpa.EntityManagerProvider;
 import org.axonframework.common.transaction.TransactionManager;
 import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventsourcing.*;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.eventsourcing.eventstore.jpa.JpaEventStorageEngine;
@@ -18,6 +19,10 @@ import org.axonframework.springboot.util.RegisterDefaultEntities;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 /**
  * 自定义的一些配置，由于 EventStorageEngine 的 Auto Config 在自定义了 eventStore 之后就不起作用了，所以这里把 JpaEventStoreAutoConfiguration 中的内容搬过来了
@@ -38,10 +43,28 @@ public class AxonContinueConfiguration {
 
     @Bean
     public CustomEventSourcingRepository<ContractAggregate> contractAggregateRepository(CustomEmbeddedEventStore eventStore,
+                                                                                        SnapshotTriggerDefinition snapshotTriggerDefinition,
                                                                                         ParameterResolverFactory parameterResolverFactory) {
         return CustomEventSourcingRepository.builder(ContractAggregate.class)
             .eventStore(eventStore)
+            .snapshotTriggerDefinition(snapshotTriggerDefinition)
             .parameterResolverFactory(parameterResolverFactory)
+            .build();
+    }
+
+    @Bean
+    public SnapshotTriggerDefinition snapshotTriggerDefinition(Snapshotter snapshotter) {
+        return new EventCountSnapshotTriggerDefinition(snapshotter, 5);
+    }
+
+    @Bean
+    public AggregateSnapshotter snapShotter(CustomEmbeddedEventStore eventStore, ParameterResolverFactory parameterResolverFactory) {
+        Executor executor = Executors.newSingleThreadExecutor();
+
+        return AggregateSnapshotter.builder()
+            .eventStore(eventStore)
+            .parameterResolverFactory(parameterResolverFactory)
+            .aggregateFactories(Collections.singletonList(new GenericAggregateFactory<>(ContractAggregate.class)))
             .build();
     }
 
