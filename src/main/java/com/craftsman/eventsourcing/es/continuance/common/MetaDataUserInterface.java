d6b9bfdf2c6542c4f4fdb05964a87fe645f6e5b1
jokefaker <jokefaker@gmail.com>
2021-12-10 10:57:02 +0800

    add doc

--- src/main/java/com/craftsman/eventsourcing/es/continuance/common/MetaDataUserInterface.java
+++ src/main/java/com/craftsman/eventsourcing/es/continuance/common/MetaDataUserInterface.java
@@ -1,9 +1,19 @@
 package com.craftsman.eventsourcing.es.continuance.common;
 
 public interface MetaDataUserInterface {
+
+    /**
+     * 用户名
+     */
     String getName();
 
+    /**
+     * 用户 id
+     */
     Long getUserId();
 
+    /**
+     * 租户 id
+     */
     Long getCustomerId();
 }
